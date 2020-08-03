ecf925cba77d4dc9266b9678fd8f5b0a6ad32da3
DNIIBOY <daniel.nettelfield@gmail.com>
2020-08-03 20:12:24 +0200

    First "complete" version of kodehusker_class.py. Has NOT been testet

--- Python/kodehusker_class.py
+++ Python/kodehusker_class.py
@@ -1,133 +1,141 @@
-#ORDHUSKER 3000
+# ORDHUSKER 3000
 
+from time import sleep
 import os
 import pyAesCrypt
+
+# Set pause time between actions (in seconds):
+sleeptime = 1
 
 os.system('mode con: cols=47 lines=20')
 
 
-class wordSaver:
+class WordSaver:
+
+    # Set buffersize for encryption and decryption
     bufferSize = 64 * 1024
-    def __init__(self, fileName):
-        if fileName[-4:] == ".txt":
-            self.fileName = fileName
+
+    # Create static method for displaying error messages
+    @staticmethod
+    def error(error):
+        return f"\nDer skete en fejl, prøv igen:\n{error}"
+
+    # Initiate the object with a filename and automate creating the filename for the encrypted file
+    def __init__(self, filename):
+        if filename[-4:] == ".txt":
+            self.fileName = filename
         else:
-            self.fileName = fileName + ".txt"
+            self.fileName = filename + ".txt"
         self.encryptetFileName = self.fileName + ".aes"
-    def error(self, error):
-      print(f"\nDer skete en fejl, prøv igen:\n{error}")
-    def changeFileName(self):
+
+    # Function for changing the filename of the object
+    def changefilename(self):
         os.system("cls")
         self.fileName = input("""
                        Ordhusker 3000
                       Indtast filnavn: """)
         return f"Successfully changed filename to {self.fileName}"
-    def emptyFile(self):
+
+    # Function for emptying the file with the filename of the object
+    def emptyfile(self):
         fil = open(self.fileName, "w")
         fil.write("")
         fil.close()
+
+    # Encrypt the file using the key entered by the user in the write() function
     def encrypt(self, key):
         try:
-            pyAesCrypt.encryptFile(self.fileName, self.encryptetFileName, key, bufferSize)
-            self.emptyFile()
-        except:
-            return error(raise)
+            pyAesCrypt.encryptFile(self.fileName, self.encryptetFileName, key, self.bufferSize)
+            self.emptyfile()
+            return "Kryptering Successfuld"
+        except Exception as e:
+            return WordSaver.error(str(e))
 
+    # Decrypt the file using the key entered by the user in the read() function.
+    # Use ValueError to detect wrong password
     def decrypt(self, key):
-      try:
-          pyAesCrypt.decryptFile(self.encryptetFileName, self.fileName, key, bufferSize)
-      except something:
-          return "Forkert Kode!"
-      except:
-          error(raise)
-
-
-
-
-
-
-file = "john.txt"
-
-cryptetFile = file + ".aes"
-
-
-
-def cryptFile(mode, key):
-    bufferSize = 64 * 1024
-    try:
-        if mode == "en":
-            pyAesCrypt.encryptFile(file, cryptetFile, key, bufferSize)
-            emptyFile()
-            return "Kryptering Succesfuld"
-        elif mode == "de":
-            pyAesCrypt.decryptFile(cryptetFile, file, key, bufferSize)
-            return "Dekryptering Succesfuld"
-        else:
-            return "ugyldig mode"
-    except:
-        return "Fejl"
-
-def write():
-    try:
-        confirm = input("\nEr du sikker, alt gemt bliver slettet (j/n): ")
-        if confirm.lower() == "j":
-            text = input("\nHvad vil du skrive?: ")
-            password = input("\nHvad skal koden være?: ")
-            fil = open(file, "w")
-            fil.write(text)
-            fil.close()
-            print("\n" + cryptFile("en", password))
-            print("Ord gemt")
-            input("\nTryk enter for hovedmenu")
-        elif confirm.lower() == "n":
-            print("Annuleret")
-        else:
-            print("Ugyldigt")
-            return write()
-    except:
-        print("Der skete en fejl")
-
-def read():
-    count = 0
-    while count<3:
         try:
-            password = input("\nHvad er koden?: ")
-            if password.lower() == "q":
-                return
-            else:
-                print("\n" + cryptFile("de", password))
-                fil = open(file, "r")
-                print("\nDit ord er: " + str(fil.read()))
+            pyAesCrypt.decryptFile(self.encryptetFileName, self.fileName, key, self.bufferSize)
+            return "Dekrypering Successfuld"
+        except ValueError:
+            return "Forkert Kode!"
+        except Exception as e:
+            return WordSaver.error(str(e))
+
+    # Function for writing to the file. Writes to .txt file and calls encrypt() function
+    # to encrypt. encrypt() then calls emptyfile() to remove any raw text from .txt file
+    def write(self):
+        try:
+            confirm = input("\nEr du sikker, alt gemt bliver slettet (j/n): ")
+            if confirm.lower() == "j":
+                text = input("\nHvad vil du skrive?: ")
+                password = input("\nHvad skal koden være?: ")
+                fil = open(self.fileName, "w")
+                fil.write(text)
                 fil.close()
-                emptyFile()
-                count = 3
+                print("\n" + self.encrypt(password))
+                print("Ord gemt")
                 input("\nTryk enter for hovedmenu")
-        except FileNotFoundError:
-            print("Forkert Kode")
-        except:
-            print("Der skete en fejl")
-            raise
-        count+=1
+            elif confirm.lower() == "n":
+                return "Annulleret"
+            else:
+                print("Ugyldigt")
+                return self.write()
+        except Exception as e:
+            WordSaver.error(str(e))
 
-fileName = input("""
-                Ordhusker 3000
-               Indtast filnavn: """)
+    # Function for reading the aes file. Calls decrypt() with user entered key for decryption
+    def read(self):
+        count = 0
+        while count < 3:
+            try:
+                password = input("\nHvad er koden?: ")
+                if password.lower() == "q":
+                    return "Quitting read mode"
+                else:
+                    print("\n" + self.decrypt(password))
+                    fil = open(self.fileName, "r")
+                    print("\nDit ord er: " + str(fil.read()))
+                    fil.close()
+                    self.emptyfile()
+                    count = 3
+                    input("\nTryk enter for hovedmenu")
+            except Exception as e:
+                return WordSaver.error(str(e))
+            count += 1
 
-word = wordSaver(fileName)
+    # Main function, calls all other functions to run the program.
+    def run(self):
+        global sleeptime
+        while True:
+            os.system("cls")
+            user = input(f"""
+                       Filnavn: {self.fileName}
 
-while True:
-    os.system("cls")
-    user = input("""
-                Ordhusker 3000
-    (S)kriv, (l)æs, (f)ilnavn eller (q)uit: """)
-    if user.lower() == "s":
-        write()
-    elif user.lower() == "l":
-        read()
-    elif user.lower() == "f":
-        word.changeFileName()
-    elif user.lower() == "q":
-        os.system("cls")
-        break
-    else:
-        print("Det er ugyldigt")
+                        Ordhusker 3000
+            (S)kriv, (l)æs, (f)ilnavn eller (q)uit: """)
+            if user.lower() == "s":
+                print(self.write())
+                sleep(sleeptime)
+            elif user.lower() == "l":
+                print(self.read())
+                sleep(sleeptime)
+            elif user.lower() == "f":
+                print(self.changefilename())
+                sleep(sleeptime)
+            elif user.lower() == "q":
+                os.system("cls")
+                break
+            else:
+                print("Ugyldigt input")
+                sleep(sleeptime)
+
+
+# Starts the program, uses user input filename to create WordSaver object.
+# Only runs if program is run as script.
+if __name__ == '__main__':
+    fileName = input("""
+                        Ordhusker 3000
+                       Indtast filnavn: """)
+    word = WordSaver(fileName)
+    word.run()
