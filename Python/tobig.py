5c39d05a21edc8bd8145e07fb9bfb8402366c5fe
DNIIBOY <daniel.nettelfield@gmail.com>
2020-10-23 12:50:07 +0200

    tobig.py 2 LINES!

--- Python/tobig.py
+++ Python/tobig.py
@@ -1,4 +1,3 @@
 import pyperclip
 
-while True:
-    pyperclip.copy(" ".join([":regional_indicator_" + i.lower() + ":" if i != " " else "   " for i in input()]))
+while True: pyperclip.copy(" ".join([":regional_indicator_" + i.lower() + ":" if i != " " else "   " for i in input()]))
