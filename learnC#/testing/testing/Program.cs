19307836c8b78313c4f9df0b2c3a5fda625ae0a1
DNIIBOY <daniel.nettelfield@gmail.com>
2021-05-10 10:14:03 +0200

    C# learning

--- learnC#/testing/testing/Program.cs
+++ learnC#/testing/testing/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace testing
+﻿using System;
+
+namespace testing
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            string msg = "HEJJJ GUSTAVVVV, DU er en ";
+            var rand = new Random();
+            while (true){
+                int num = rand.Next(1, 20);
+                string res = msg + Convert.ToString(num) + "/10";
+                Console.ReadKey();
+                Console.WriteLine(res);
+            }
         }
     }
 }
