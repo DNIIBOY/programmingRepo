e58483ab19475e752ffc3add596698fd54a0b587
DNIIBOY <daniel.nettelfield@gmail.com>
2021-06-08 16:35:52 +0200

    fixFormat.py Capitalization

--- Python/fixFormat.py
+++ Python/fixFormat.py
@@ -10,6 +10,8 @@ while True:
             if not x[0].isalnum():
                 x = x[1:]
             else:
+                if x[0].isalpha():
+                    x = x[0].capitalize() + x[1:]
                 break
         pyperclip.copy(x)
         print("After: ", x)
