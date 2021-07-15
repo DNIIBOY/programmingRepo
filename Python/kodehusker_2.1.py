041e95e8d6b29566ca925b21a95060fd4dd425c8
DNIIBOY <daniel.nettelfield@gmail.com>
2021-07-15 22:18:25 +0200

    Bedre kommentarere

--- Python/kodehusker_2.1.py
+++ Python/kodehusker_2.1.py
@@ -6,10 +6,13 @@ width, height = (275, 270)
 
 
 def prep_vp(wide: int, high: int):
+    # Denne funktion forbereder selve vinduet, som alt det andet bliver sat ind i
     vp = dpg.create_viewport(title='Kodehusker v2.1', width=wide, height=high)
     dpg.set_viewport_maximized_box(False)
+    # Kigger i brugerens "billeder" mappe efter en icon fil
     dpg.set_viewport_small_icon(f"C:\\Users\\{os.getenv('username')}\\Pictures\\darkLock.ico")
 
+    # Låser viewport til bestemt størrelse
     dpg.set_viewport_max_width(wide)
     dpg.set_viewport_min_width(wide)
     dpg.set_viewport_max_height(high)
@@ -27,6 +30,7 @@ def find_aes_file() -> str:
 
 
 class WordSaver:
+
     def __init__(self):
 
         self.bufferSize = 64 * 1024
@@ -47,10 +51,10 @@ class WordSaver:
             dpg.add_button(label="Skriv", callback=self.toggle_write_window)
             dpg.add_same_line(spacing=10)
             dpg.add_button(label="Læs", callback=self.toggle_read_window)
-            self.statusGood = dpg.add_text("", color=[0, 255, 0], show=False, pos=[10, 70])
-            self.statusBad = dpg.add_text("", color=[255, 0, 0], show=False, pos=[10, 70])
+            self.statusGood = dpg.add_text("", color=[0, 255, 0], show=False, pos=[10, 70])  # 2 status textfelter
+            self.statusBad = dpg.add_text("", color=[255, 0, 0], show=False, pos=[10, 70])  # forskellige farver
             dpg.add_text("Text der skal krypteres:", pos=[40, 85])
-            # Dette textfelt bruges til Input og Output til filen
+            # Dette textfelt bruges til Input og Output til den krypterede fil
             self.rwfield = dpg.add_input_text(multiline=True, width=width - 50, height=90, pos=[15, 110], label='')
             dpg.add_button(label="Clear", pos=[100, 203], callback=lambda: dpg.set_value(self.rwfield, ""))
 
@@ -83,11 +87,14 @@ class WordSaver:
         self.fileName = self.fileName + ".txt"  # Sætter rigtige filnavne med typer
         self.encryptetFileName = self.fileName + ".aes"  # .aes er krypteringstypen
 
-    def show_error(self, error):
+    def show_error(self, error: str):
+        # Funktion der opretter tekstfelt og skriver en fejlkode, hvis der er noget galt
         dpg.add_spacing(count=35, parent=self.mainWindow)
         dpg.add_text(error, color=[255, 0, 0], parent=self.mainWindow)
 
     def show_status(self, status: str, good: bool):
+        # Funktion der viser et af status tekstfelterne og sætter "status" ind i det
+        # hvis good er True viser den det grønne felt. Ellers det røde. Slukker også modsatte felt
         if good:
             dpg.show_item(self.statusGood)
             dpg.hide_item(self.statusBad)
@@ -111,7 +118,7 @@ class WordSaver:
             dpg.hide_item(self.writeWindow)  # Skjuler modsatte vindue hvis åbnet
 
     def toggle_read_window(self, mode: bool = True):
-        # Skjuler / viser vindet til at læse fra filen
+        # Skjuler / viser vindet til at læse fra krypteret fil
         if mode:
             dpg.show_item(self.readWindow)
             dpg.hide_item(self.writeWindow)
@@ -123,50 +130,56 @@ class WordSaver:
         os.remove(self.fileName)
 
     def encrypt(self, key: str) -> bool:
+        # Denne funktion bruger self.fileName som .txt og kryptere den, til en .txt.aes fil
+        # Bruger key parameter til som krypteringsnøgle
         try:
             pyAesCrypt.encryptFile(self.fileName, self.encryptetFileName, key, self.bufferSize)
-            self.delete_txt()
-            return True
+            self.delete_txt()  # Her slettes den originale .txt fil, så der ikke er noget ukrypteret
+            return True  # Retunere True hvis der ikke opstod nogle exceptions
         except Exception as e:
             self.show_error(str(e))
             return False
 
     def decrypt(self, key: str) -> bool:
+        # Denne funktion tager self.encryptetFileName som er .txt.aes og omdanner den til .txt
+        # uden kryptering. Bruger ket parameter som dekrypteringsnøgle
         try:
             pyAesCrypt.decryptFile(self.encryptetFileName, self.fileName, key, self.bufferSize)
-            return True
-        except ValueError:
-            self.show_status("Forkert adgangskode blev angivet", False)
+            return True  # Retunere True hvis succesfuldt
+        except ValueError:  # pyAesCrypt raiser ValueError, hvis dekrypteringsnøglen er forkert
+            self.show_status("Forkert adgangskode blev angivet", False)  # Viser til brugeren hvis koden er forkert
             return False
-        except Exception as e:
+        except Exception as e:  # Alle andre exceptions bliver vist som en fejl
             self.show_error(str(e))
             return False
 
     def write_text(self):
-        with open(self.fileName, "w") as f:
-            f.write(dpg.get_value(self.rwfield))
+        # Bruger encrypt funktionen til at kryptere brugerens indtastning
+        with open(self.fileName, "w") as f:  # Åbner eksisterende, eller opretter ny .txt fil under navnet fileName
+            f.write(dpg.get_value(self.rwfield))  # Skriver brugerens input i txt filen og gemmer den
         password = dpg.get_value(self.writePW)
-        if self.encrypt(password):
-            self.show_status("Text succesfult krypteret og gemt", True)
-        dpg.set_value(self.writePW, "")
+        if self.encrypt(password):  # Tjekker om der blev krypteret succesfult
+            self.show_status("Text succesfult krypteret og gemt", True)  # Fortæller brugeren om det var succesfult
+        dpg.set_value(self.writePW, "")  # Tømmer brugerens inputfelt og lukker skrivevinduet
         dpg.hide_item(self.writeWindow)
 
     def read_text(self):
+        # Bruger decrypt funktionen til at forsøge at dekryptere fileName.txt filen
         password = dpg.get_value(self.readPW)
-        if self.decrypt(password):
-            with open(self.fileName, "r") as f:
-                dpg.set_value(self.rwfield, f.read())
-            self.delete_txt()
+        if self.decrypt(password):  # Tjekker om dekryptering virkede
+            with open(self.fileName, "r") as f:  # Åbner dekryptet fil
+                dpg.set_value(self.rwfield, f.read())  # Viser indholdet til brugeren
+            self.delete_txt()  # Sletter dekrypteret fil, så indholdet forbliver hemmeligt
             self.show_status("Fil dekrypteret succesfult!", True)
-        dpg.set_value(self.readPW, "")
+        dpg.set_value(self.readPW, "")  # Tømmer inputfelt og lukker læsevinduets
         dpg.hide_item(self.readWindow)
 
 
-if __name__ == '__main__':
-    w = WordSaver()
-    view = prep_vp(width, height)
+if __name__ == '__main__':  # Koden kører ikke, hvis importeret.
+    w = WordSaver()  # Opretter WordSaver objekt
+    view = prep_vp(width, height)  # Forbereder viewport
     dpg.setup_dearpygui(viewport=view)
-    dpg.set_primary_window(w.mainWindow, True)
+    dpg.set_primary_window(w.mainWindow, True)  # Sætter hovedvinduet som hovedvindue
 
     dpg.show_viewport(view)
-    dpg.start_dearpygui()
+    dpg.start_dearpygui()  # Starter brugerfladen
