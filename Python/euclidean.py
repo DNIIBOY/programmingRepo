6bab454b2a56c47b3d33aaca0c878228e2aa17ee
DNIIBOY <daniel.nettelfield@gmail.com>
2023-05-23 13:29:50 +0200

    remove dumb else

--- Python/euclidean.py
+++ Python/euclidean.py
@@ -1,9 +1,8 @@
 def extended_gcd(a, b) -> tuple:
     if a == 0:
         return b, 0, 1
-    else:
-        gcd, x, y = extended_gcd(b % a, a)
-        return gcd, y - (b // a) * x, x
+    gcd, x, y = extended_gcd(b % a, a)
+    return gcd, y - (b // a) * x, x
 
 
 def display(items: dict) -> None:
